7c8970d73c2ebadfaa862a6422102f6d848c98bc
tors42 <4084220+tors42@users.noreply.github.com>
2023-09-23 19:16:32 +0200

    Bump dep to 0.0.75

--- src/main/java/com/example/Main.java
+++ src/main/java/com/example/Main.java
@@ -11,11 +11,6 @@ public class Main {
 
     var client = Client.basic();
 
-    // List<User> list = client.bot().botsOnline(5).stream()
-    // .toList();
-    //
-    // list.forEach(System.out::println);
-
     String message = client.teams().byTeamId("lichess-swiss")
         .map(team -> "Team %s has %d members!".formatted(team.name(), team.nbMembers()))
         .orElse("Didn't find team");
