7ad1165b6d41c2526402886762d06a055c1ead34
tors42 <4084220+tors42@users.noreply.github.com>
2025-11-16 14:49:48 +0100

    Update example Java 25 style
    - instance main method
    - java.lang.IO

--- src/main/java/com/example/Main.java
+++ src/main/java/com/example/Main.java
@@ -4,13 +4,13 @@ import chariot.Client;
 
 public class Main {
 
-    public static void main(String[] args) {
+    void main() {
         var client = Client.basic();
 
         String message = client.teams().byTeamId("lichess-swiss").maybe()
             .map(team -> "Team %s has %d members!".formatted(team.name(), team.nbMembers()))
             .orElse("Didn't find team");
 
-        System.out.println(message);
+        IO.println(message);
     }
 }
